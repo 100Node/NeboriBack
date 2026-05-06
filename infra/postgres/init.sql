559be408f2c9eacafead0b6f70fe888fcaa0b6ca
OleksiTK1 <oleksiy_t@yr-solutions.team>
2026-05-06 23:45:31 +0300

    make normal services

--- infra/postgres/init.sql
+++ infra/postgres/init.sql
@@ -1,4 +1,6 @@
 -- infra/postgres/init.sql
+CREATE DATABASE nebori_auth;
+CREATE DATABASE nebori_user;
 CREATE DATABASE auth_db;
 CREATE DATABASE video_metadata_db;
 CREATE DATABASE upload_tasks_db;
